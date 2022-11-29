39f9f3c4d1543ca146b318c808f7b9cadfb8c244
Emily Prud'hommeaux <prudhome@bc.edu>
2022-11-29 00:26:58 -0500

    Add files via upload

---
+++ part3-solution.py
@@ -0,0 +1,5 @@
+firstnames = ["Lewis", "Sergio", "Donglai", "Howard", "Jose"]
+colors = ["red", "blue", "blue", "green", "brown"]
+
+for f, c in zip(firstnames, colors):
+    print(f"{f}'s favorite color is {c}")
